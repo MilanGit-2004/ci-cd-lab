e2088f31dd79c30cedad7b9a93913b6056f93908
MilanAlgama <milanharsha28@gmail.com>
2026-05-24 22:13:07 +0530

    Add Substraction function to the Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,12 @@
 
 public class Calculator {
+    //Addition
     public int add(int a, int b){
         return a+b;
+    }
+
+    //Substraction
+    public int sub(int a, int b){
+        return a-b;
     }
 }
--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,10 +1,19 @@
 public class CalculatorTest {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
+
+        //Addition
         if(cal.add(2, 3) == 5){
-            System.out.println("Test Passed");
+            System.out.println("Addition Test Passed");
         }else{
-            System.out.println("Test Failed");
+            System.out.println("Addition Test Failed");
+        }
+
+        //Substractiom
+        if(cal.sub(6, 10) == 4){
+            System.out.println("Substraction Test Passed");
+        }else{
+            System.out.println("Substraction Test Failed");
         }
     }
 }
